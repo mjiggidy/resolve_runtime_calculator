517f1a31b579c8c22e26206a22c7a68a2ae15248
Michael Jordan <michael@glowingpixel.com>
2026-09-20 13:42:37 -0700

    Here goes nothin

---
+++ installers/windows/action_build_installer.sh
@@ -0,0 +1,33 @@
+# NOTE: Run this from installers/macos
+
+VERSION="${1#v}"
+INSTALLATION_DEST="/Library/Application Support/Blackmagic Design/DaVinci Resolve/Workflow Integration Plugins"
+REVERSE_DOMAIN="com.glowingpixel.runtimecalculator"
+
+DIST_STAGING="pkg_staging"
+DIST_BASE="$DIST_STAGING/$REVERSE_DOMAIN"
+DIST_LIB="$DIST_BASE/lib"
+
+DIST_PKG="dist"
+
+PACKAGE_NAME="runtimecalculator_v${VERSION}_windows.exe"
+
+echo "Dir looks like:"
+ls -la
+
+mkdir -p "$DIST_PKG"
+mkdir -p "$DIST_LIB"
+
+echo "Staging python package to $DIST_STAGING"
+pip install . --target "$DIST_LIB"
+cp workflow_integration/Runtime\ Calculator.py "$DIST_STAGING/"
+
+echo "Building installer"
+iscc /DMyAppVersion="$VERSION" --output-dir="$DIST_PKG" --output-filename="$PACKAGE_NAME" installer.iss
+
+DIST_PKG_FULL="$(realpath "$DIST_PKG/$PACKAGE_NAME")"
+
+echo "Done!  Written to $DIST_PKG_FULL"
+
+# Write it to the env
+echo "PKG_PATH=$DIST_PKG_FULL" >> "$GITHUB_ENV"
